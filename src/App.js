65877b954a65e9876c841266ca523474540888a5
hadayethm <hadyethm1999@gmail.com>
2024-08-19 17:40:31 +0600

    update 3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
+// import Header
 import Header from "./layout/Header";
-
+// import HomeOne
 import HomeOne from "./page/Home";
-
+// import AboutPage
 import AboutPage from "./page/About";
-
+// import EventPageArea
 import EventPageArea from "./page/Event";
-
 // import Event Details Page
 import EventDetails from "./page/EventDetails";
 // import Event Booking Page
 import EventBookingPage from "./page/EventBooking";
+// import TeamDetailsPage Page
 import TeamDetailsPage from "./page/TeamDetails";
+// import TeamPage Page
 import TeamPage from "./page/Team";
-// import PortfolioPage
+// import Portfolio Page
 import PortfolioPage from "./page/Portfolio";
+// import rtfolioDetails Page
 import PortfolioDetailsPage from "./page/PortfolioDetails";
+// import Gallery Page 
 import GalleryPage from "./page/Gallery";
+// import Service Page
 import ServicePage from "./page/Service"
+// import ServiceDetails Page
 import ServiceDetailsPage from "./page/ServiceDetails"
+// import Faqs Page
 import FaqsPage from "./page/Faq";
+// import Testimonial Page
 import TestimonialPage from "./page/Testimonial";
+// import Ticket Page
 import TicketPage from "./page/Ticket";
+// import Product Page
 import ProductPage from "./page/Product";
+// import ProductDetails Page
 import ProductDetailsPage from "./page/ProductDetails";
+// import Cart Page
 import CartPage from "./page/Cart";
+// import Checkout Page
 import CheckoutPage from "./page/Checkout";
+// import Login Page
 import LoginPage from "./page/Login";
+// import Error Page
 import ErrorPage from "./page/Error";
+// import ComingSoon Page
 import ComingSoonPage from "./page/ComingSoon";
+// import News Page
 import NewsPage from "./page/News";
+// import NewsDetails Page
 import NewsDetailsPage from "./page/NewsDetails";
+// import Portfolio Page
 import ContactPage from "./page/Contact";
+// import Portfolio Page
 
 
 
@@ -75,7 +94,6 @@ const App = () => {
         <Route path="/news-details" element={<NewsDetailsPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="*" element={<ErrorPage />} />
-        
       </Routes> 
       <InstagramSlider/>
      <FooterArea/>
